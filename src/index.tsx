6b7ac8cd6df49ff89fb31e50a6cf096163092786
abdullah <abdullah29dev@gmail.com>
2024-08-23 16:32:21 +0500

    fix: 0.1.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,13 @@
+import { Text, View } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function MyCustomText(a: number, b: number): any {
+  return (
+    <View>
+      <Text>{a * b}</Text>
+    </View>
+  );
 }
